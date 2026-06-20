cc234e5ed47e8cbf4e76049231fe34a27bb4bad7
Arlan Neres <arlanneresdossantos@gmail.com>
2026-06-20 20:37:01 -0300

    solution 12

--- services/order.py
+++ services/order.py
@@ -23,8 +23,7 @@ def create_order(
 
     with transaction.atomic():
         if date:
-            parsed_date = datetime.datetime.strptime(
-                date, "%Y-%m-%d %H:%M")
+            parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
             order = Order.objects.create(user=user)
             Order.objects.filter(pk=order.pk).update(created_at=parsed_date)
             order.refresh_from_db()

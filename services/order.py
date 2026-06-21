416926f0deafa5054a6fc627535c96113f1ed803
Arlan Neres <arlanneresdossantos@gmail.com>
2026-06-20 21:24:54 -0300

    solution 17

--- services/order.py
+++ services/order.py
@@ -23,19 +23,11 @@ def create_order(
 ) -> Order:
     user = get_object_or_404(User, username=username)
 
-    field = Order._meta.get_field("created_at")
-
-    auto_now_add_original = getattr(field, "auto_now_add", False)
-
-    try:
-        if date:
-            parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
-            setattr(field, "auto_now_add", False)
-            order = Order.objects.create(user=user, created_at=parsed_date)
-        else:
-            order = Order.objects.create(user=user)
-    finally:
-        setattr(field, "auto_now_add", auto_now_add_original)
+    order = Order.objects.create(user=user)
+    if date:
+        parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
+        Order.objects.filter(pk=order.pk).update(created_at=parsed_date)
+        order.refresh_from_db()
 
     for ticket_data in tickets:
         Ticket.objects.create(
